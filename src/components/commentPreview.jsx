11e2483d9223b7fb2e5974dd00a240133caba99a
Namuun Togoobor <namuu1214@gmail.com>
2021-06-12 00:55:44 +0800

    medkue comment l ym shg bn

--- src/components/commentPreview.jsx
+++ src/components/commentPreview.jsx
@@ -1,34 +1,95 @@
-import { Card, CardContent, Typography } from '@material-ui/core'
-
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Slider from "react-slick";
+import Grid from "@material-ui/core/Grid";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 const CommentPreview = () => {
-    const comments = [
-        {
-            poster: 'poster1',
-            comment: 'comment1'
-        },
-        {
-            poster: 'poster2',
-            comment: 'comment2'
-        },
-        {
-            poster: 'poster3',
-            comment: 'comment3'
-        }
-    ]
-    return <>
-        {comments.map(cmt => (
-            <Card>
-                <CardContent>
-                    <Typography>
-                        {cmt.poster}
-                    </Typography>
-                    <Typography>
-                        {cmt.comment}
-                    </Typography>
-                </CardContent>
-            </Card>
-        ))}
-    </>
-}
+  console.log(window.innerWidth > 600);
+  const useStyles = makeStyles({
+    container: {
+      display: "flex",
+      justifyContent: "space-around",
+      alignItems: "center",
+      flexDirection: window.innerWidth < 600 ? "column" : "row",
+    },
+    root: {
+      margin: 5,
+      backgroundColor: "#fcba03",
+      width: 300,
+    },
 
-export default CommentPreview
+    title: {
+      fontSize: 14,
+    },
+  });
+  const comments = [
+    {
+      poster: "Ээж нь",
+      comment: "Охиндоо маш их хайртай шүү. Сайн хүн болоод балбллбла",
+    },
+    {
+      poster: "Аав нь",
+      comment: "ХАхаххахахахохоох хохохохухухху хухух хүхүхүх хихихих",
+    },
+    {
+      poster: "Аав нь",
+      comment: "ХАхаххахахахохоох хохохохухухху хухух хүхүхүх хихихих",
+    },
+    {
+      poster: "Аав нь",
+      comment: "ХАхаххахахахохоох хохохохухухху хухух хүхүхүх хихихих",
+    },
+    {
+      poster: "Авга ах Намуунаа",
+      comment: "Хэлэх үг алга",
+    },
+  ];
+  const classes = useStyles();
+  const sliderSettings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "0",
+    dots: true,
+    speed: 500,
+    slidesToShow: 2,
+    // slidesToScroll: 1,
+    // autoplaySpeed: 3000,
+    // autoplay: true,
+    easing: "linear",
+    pauseOnFocus: true,
+    pauseOnHover: true,
+  };
+  return (
+    <Slider {...sliderSettings}>
+      {comments.map((cmt) => (
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              {cmt.poster}
+            </Typography>
+
+            <Typography className={classes.pos} color="textSecondary">
+              {cmt.comment}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      ))}
+    </Slider>
+  );
+};
+
+export default CommentPreview;
